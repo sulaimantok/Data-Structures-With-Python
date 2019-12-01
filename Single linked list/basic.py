8384cb53d1e0daf3985058daaf14c9b34c0fe4a9
sulaimantok <sulaimanniceguy@gmail.com>
2019-12-01 08:27:40 +0700

    add Rotate function to single linked list

--- Single linked list/basic.py
+++ Single linked list/basic.py
@@ -265,3 +265,25 @@ class LinkedList:
               return 1 + self.count_occurences_recursive(node.next, data)
           else:
               return self.count_occurences_recursive(node.next, data)
+#Rotate
+        def rotate(self,k):
+            if self.head and self.head.next:
+                p = self.head
+                q = self.head
+                prev = None
+                count = 0
+
+                while p and count < k:
+                    prev = p
+                    p = p.next
+                    q = q.next
+                    count += 1
+                p = prev
+                while q:
+                    prev = q
+                    q = q.next
+                q = prev
+
+                q.next = self.head
+                self.head = p.next
+                p.next = None
