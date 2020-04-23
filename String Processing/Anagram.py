ec515290bb0f2f96595435d054410af8ebdb02c7
SULAIMAN <sulaimanniceguy@gmail.com>
2020-04-23 19:58:14 +0700

    String Processing -> is Anagram

---
+++ String Processing/Anagram.py
@@ -0,0 +1,28 @@
+def is_anagram(s1, s2):
+    ht = dict()
+
+    if len(s1) != len(s2):
+        return False
+
+    for i in s1:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+    for i in s2:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    return True
+
+s1 = "fairy tales"
+s2 = "rail safety"
+## normalizing the strings
+s1 = s1.replace(" ", "").lower()
+s2 = s2.replace(" ", "").lower()
+
+print(is_anagram(s1, s2))
